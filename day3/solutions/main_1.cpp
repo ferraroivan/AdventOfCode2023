53c8f2e570fee89b6baa654c0ebabc377e55b899
Ivan Ferraro <ivan@lightos.home>
2023-12-03 17:35:30 +0100

    day3 AoC

---
+++ day3/solutions/main_1.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <fstream>
+#include <string>
+#include <vector>
+
+struct Number {
+    int col;
+    int row;
+    int number;
+    int order;
+};
+
+int main(int argc, char * argv[]) {
+    
+    std::string input = "../input/input_day3.txt";
+    
+    std::string line;
+    std::fstream file(input);
+
+    std::vector<std::string> vec;
+    std::vector<Number> numbers;
+
+    while (std::getline(file, line)) {
+        vec.push_back(line);
+        bool is_number = false;
+        int number = -1;
+        int start = 0;
+        int order = 0;
+        
+        // Lambda function
+        const auto add_util = [&]() {
+            Number n;
+            n.row = vec.size() - 1;
+            n.col = start;
+            n.number = number;
+            n.order = order;
+            numbers.push_back(n);
+            number = -1;
+            start = 0;
+            order = 0;
+        };
+
+        for (int i = 0; i < line.size(); i++) {
+            const char c = line[i];
+            if (c >= '0' && c <= '9') {
+                if (is_number == false) {
+                    number = 0;
+                    start = i;
+                    order = 0;
+                }
+                is_number = true;
+                number = (number * 10) + int (c - '0');
+                order++;
+            } else {
+                is_number = false;
+            }
+            if (number != -1 && !is_number) {
+                add_util();
+            }
+        }
+        if (number != -1) {
+            add_util();
+        }
+    }
+    std::vector<int> parts;
+    for (const auto& n : numbers) {
+        bool still_searching = true;
+        for (int row = n.row - 1; (row <= n.row + 1) && still_searching; row++) {
+            if (row < 0 || row >= vec.size()) {
+                continue;
+            }
+
+            const int col_increment = (row == n.row) ? n.order + 1 : 1;
+            for (int col = n.col - 1; (col <= n.col + n.order) && still_searching; col += col_increment) {
+                if (col < 0 || col >= vec[row].size()) {
+                    continue;
+                }
+                if (vec[row][col] != '.' && !(vec[row][col] >= '0' && vec[row][col] <= '9')) {
+                    still_searching = false;
+                    parts.push_back(n.number);
+                }
+            }
+        }
+    }
+
+    const auto total = std::accumulate(std::begin(parts), std::end(parts), 0);
+    std::cout << total << std::endl;
+    return 0;             
+}
+
+// 525181
